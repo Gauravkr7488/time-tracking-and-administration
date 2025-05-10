0321d9a1de668e30a992ac065c0808092dfa2c39
Gaurav Kumar <gauravkr748800@gmail.com>
2025-05-10 23:02:31 +0530

    Refactoring: Validator.ts

--- src/Validator.ts
+++ src/Validator.ts
@@ -2,34 +2,42 @@ import * as vscode from 'vscode';
 import { Message } from './VsCodeUtils';
 
 
-export class ValidateAndGet {
+export class ActiveDocAndEditorUtils {
     private message = new Message();
-    document?: vscode.TextDocument;
-    editor?: vscode.TextEditor;
 
     getActiveDoc() {
-        let editor = this.editor;
-        editor = vscode.window.activeTextEditor;
-        if (!editor) {
+        const activeEditor = this.getActiveEditor();
+        if (!activeEditor) {
             this.message.err("No active text-editor");
             return;
         }
-        this.editor = editor;
-        this.document = vscode.window.activeTextEditor?.document;
-        return this.document;
+        const activeDocument = activeEditor.document;
+        return activeDocument;
+    }
+
+    getActiveEditor() {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            this.message.err("No active text-editor");
+            return;
+        }
+        return activeEditor;
     }
 
     isThisYamlDoc(): boolean {
-        this.getActiveDoc();
-        if (!this.document || this.document.languageId !== "yaml") {
-            vscode.window.showErrorMessage("This command only works with YAML files.");
+        const activeDocument = this.getActiveDoc();
+        if(!activeDocument) return false;
+        if (activeDocument.languageId !== "yaml") {
+            this.message.err("This command only works with YAML files.");
             return false;
         }
         return true
     }
 
     getCursorPosition() { // place this into misc
-        if (!this.editor) return;
-        return this.editor.selection.active;
+        const activeEditor = this.getActiveEditor();
+        if(!activeEditor) return;
+        const cursorPosition = activeEditor.selection.active;
+        return cursorPosition;
     }
 }
