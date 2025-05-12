eb5eafe1b239f1cef5b0c16fa519b111a74da142
Gaurav Kumar <gauravkr748800@gmail.com>
2025-05-12 11:21:12 +0530

    Refactoring: class Data 1. added things

--- src/Data.ts
+++ src/Data.ts
@@ -1,10 +1,23 @@
-export class Data{
+export class Data {
     public static readonly MESSAGES = {
         ERRORS: {
+            NOT_A_PROPER_TASK: "This is not a proper task as it does not have any items inside it",
+            FAILED_TO_PARSE_YAML: "Failed to parse YAML",
+            NO_ACTIVE_TASK: "There is no active task",
+            RUN_SPECIFY_SR_FIRST: "run specify Standup report first",
+            NO_SR_CODE: "There is no SR Code under the cursor",
             NO_ACTIVE_TEXT_EDITOR: "No active text editor.",
             NO_WORD_AT_CURSOR: "No word found at cursor position. Place cursor on a YAML key.",
             NO_LINK_FOUND: "No link found containing the cursor.",
-            THIS_COMMAND_ONLY_WORKS_WITH_YAML_FILES : "This command only works with YAML files."
+            THIS_COMMAND_ONLY_WORKS_WITH_YAML_FILES: "This command only works with YAML files."
+        },
+        INFO: {
+            COPIED_TO_CLIPBOARD: (something: string) => `'${something}' copied to your clipboard`,
+            TIMER_RESUMED: "Timer resumed.",
+            TIMER_PAUSED: "Timer paused.",
+            TIMER_STOPPED: (durationMinutes: number) => `Timer stopped. Duration: ${durationMinutes} minutes`,
+            TASK_SELECTED: (f2YamlLink: string) => `The timer has started on Task: ${f2YamlLink}`,
+            SR_SPECIFIED: (srCode: string) => `${srCode} is selected as the Standup Report. Please select a Task and issue the Start timer on Task command`
         }
     }
 
@@ -13,14 +26,25 @@ export class Data{
         CAPTURED_DOCUMENT_URI: "capturedDocumentUri",
         DETECTED_YAML_LINK: "detectedYamlLink"
     }
-    
+
     public static readonly REGEX_PATTERNS = {
-        LINK: /-->.*<\:/g,
-        COLON: /:$/
+        LINK: /-->.*</,
+        COLON: /:$/,
+        REFERENCE: /\$@.*@\$/g
     }
 
     public static readonly MISC = {
-        YAML: "yaml"
+        EMPTY_STRING: "",
+        YAML: "yaml",
+        EXTENSION_NAME: "time-tracking-and-administration"
     }
 
+    public static readonly TIME_KEYS = {
+        START_TIME_KEY: 'timerStartTime',
+        ACCUMULATED_TIME_KEY: 'timerAccumulatedTime',
+        IS_PAUSED_KEY: 'timerIsPaused',
+        START_TIME_ISO_KEY: 'timerStartTimeISO',
+        PAUSE_RESUME_STATUS_KEY: 'timerPauseResumeStatus',
+        DURATION_MINUTES_KEY: 'timerDurationMinutes'
+    }
 }
