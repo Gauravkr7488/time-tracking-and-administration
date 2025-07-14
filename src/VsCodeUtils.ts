33e2f09d0469b19e31d9488ae19cfc22bb5772b2
Gaurav Kumar <gauravkr748800@gmail.com>
2025-07-14 14:49:54 +0530

    Cleaned VsCodeUtils.ts

--- src/VsCodeUtils.ts
+++ src/VsCodeUtils.ts
@@ -6,24 +6,32 @@ import path from 'path';
 export class VsCodeUtils {
 
     static async getFileUri(filePath: any): Promise<vscode.Uri> {
-        let fileUri;
-        const config = vscode.workspace.getConfiguration(Data.MISC.EXTENSION_NAME);
-        const rootPath = VsCodeUtils.getRootPath(config); // todo 
+        let fileUri: vscode.Uri;
+        const rootPath = VsCodeUtils.getRootPath();
         try {
             const filePathFromRoot = rootPath + filePath + ".yaml";
             fileUri = vscode.Uri.file(path.resolve(filePathFromRoot));
             await vscode.workspace.fs.stat(fileUri);
         } catch (error) {
-            const filePathFromRoot = rootPath + filePath + ".yml";
-            fileUri = vscode.Uri.file(path.resolve(filePathFromRoot));
-            await vscode.workspace.fs.stat(fileUri);
+            try {
+                const filePathFromRoot = rootPath + filePath + ".yml";
+                fileUri = vscode.Uri.file(path.resolve(filePathFromRoot));
+                await vscode.workspace.fs.stat(fileUri);
+            } catch (e) {
+                throw new Error(`${filePath} not found`);
+            }
         }
         return fileUri;
     }
 
-    private static getRootPath(config: vscode.WorkspaceConfiguration) {
+    static getConfig() {
+        return vscode.workspace.getConfiguration(Data.MISC.EXTENSION_NAME);
+    }
+
+    private static getRootPath() {
+        const config = this.getConfig();
         let rootPath = config.get<string>('rootPath');
-        if (!rootPath) throw new Error("root path is not set"); // todo 
+        if (!rootPath) throw new Error(Data.MESSAGES.ERRORS.NO_ROOT_PATH);
         let lastChar = rootPath[rootPath.length - 1]
         if (lastChar != "\\") rootPath = rootPath + "\\";
         return rootPath;
@@ -31,26 +39,17 @@ export class VsCodeUtils {
 
     static getActiveDoc() {
         const activeEditor = this.getActiveEditor();
-        if (!activeEditor) {
-            Message.err(Data.MESSAGES.ERRORS.NO_ACTIVE_TEXT_EDITOR);
-            return;
-        }
-        const activeDocument = activeEditor.document;
-        return activeDocument;
+        return activeEditor.document;
     }
 
     static getActiveEditor() {
         const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor) {
-            Message.err(Data.MESSAGES.ERRORS.NO_ACTIVE_TEXT_EDITOR);
-            return;
-        }
+        if (!activeEditor) throw new Error(Data.MESSAGES.ERRORS.NO_ACTIVE_TEXT_EDITOR);
         return activeEditor;
     }
 
     static isThisYamlDoc(): boolean {
         const activeDocument = this.getActiveDoc();
-        if (!activeDocument) return false;
         if (activeDocument.languageId !== Data.MISC.YAML) {
             Message.err(Data.MESSAGES.ERRORS.THIS_COMMAND_ONLY_WORKS_WITH_YAML_FILES);
             return false;
@@ -60,12 +59,7 @@ export class VsCodeUtils {
 
     static getCursorPosition() {
         const activeEditor = this.getActiveEditor();
-        if (!activeEditor) {
-            Message.err(Data.MESSAGES.ERRORS.NO_ACTIVE_TEXT_EDITOR);
-            return;
-        }
-        const cursorPosition = activeEditor.selection.active;
-        return cursorPosition;
+        return activeEditor.selection.active;
     }
 
     static sleep(ms: number): Promise<void> {
